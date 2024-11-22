eb20ac4d83857bda2f1a262cd2487fdd7d11bd4e
Nicolas Sepulveda <nicolas.sepulveda.b@usach.cl>
2024-11-21 21:19:27 -0300

    Repo and Service for Orden

--- src/main/java/com/example/ecommerce/services/OrdenService.java
+++ src/main/java/com/example/ecommerce/services/OrdenService.java
@@ -1,13 +1,51 @@
 package com.example.ecommerce.services;
 
+import com.example.ecommerce.entities.OrdenEntity;
 import com.example.ecommerce.repositories.OrdenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OrdenService {
     @Autowired
     OrdenRepository ordenRepository;
 
+    public OrdenEntity getOrdenById(Long id) {
+        OrdenEntity orden  = ordenRepository.findById(id);
+        if (orden == null) {
+            throw new RuntimeException("No existe la orden");
+        }
+        return orden;
+    }
+
+    public List<OrdenEntity> getAllOrdenes() {
+        return ordenRepository.findAll();
+    }
+
+    public void saveOrden(OrdenEntity orden) {
+        ordenRepository.save(orden);
+    }
+
+    public void updateOrden(OrdenEntity orden) {
+        ordenRepository.update(orden);
+    }
+
+    public void deleteOrdenById(Long id) {
+        OrdenEntity orden = ordenRepository.findById(id);
+        if (orden == null) {
+            throw new RuntimeException("No existe la orden");
+        }
+        ordenRepository.deleteById(id);
+    }
+
+    public void deleteOrden(OrdenEntity orden) {
+        OrdenEntity existing = ordenRepository.findById(orden.getIdOrden());
+        if (existing == null) {
+            throw new RuntimeException("No existe la orden");
+        }
+        ordenRepository.delete(orden);
+    }
 
 }
