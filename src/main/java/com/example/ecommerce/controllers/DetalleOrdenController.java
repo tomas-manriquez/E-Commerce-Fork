f61340cbec3ea44536ff2002461a81f61f3613c6
tomas-manriquez <tomas.manriquez@usach.cl>
2024-11-21 21:29:32 -0300

    DetalleOrdenController + OrdenController + ProductoController

--- src/main/java/com/example/ecommerce/controllers/DetalleOrdenController.java
+++ src/main/java/com/example/ecommerce/controllers/DetalleOrdenController.java
@@ -1,9 +1,73 @@
 package com.example.ecommerce.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.ecommerce.entities.DetalleOrdenEntity;
+import com.example.ecommerce.services.DetalleOrdenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/detalles")
 public class DetalleOrdenController {
+    @Autowired
+    DetalleOrdenService detalleOrdenService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<DetalleOrdenEntity>> getAllDetalles() {
+        List<DetalleOrdenEntity> det = detalleOrdenService.getAllDetalles();
+        if (det != null) {
+            return ResponseEntity.ok(det);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/byId/{id}")
+    public ResponseEntity<DetalleOrdenEntity> getDetalleById(@PathVariable Long id) {
+        DetalleOrdenEntity det = detalleOrdenService.getDetalleById(id);
+        if (det != null) {
+            return ResponseEntity.ok(det);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<String> saveDetalle(@RequestBody DetalleOrdenEntity det) {
+        detalleOrdenService.saveDetalle(det);
+        return ResponseEntity.status(HttpStatus.CREATED).body("DetalleOrdenEntity created successfully");
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<String> updateDetalle(@RequestBody DetalleOrdenEntity det) {
+        detalleOrdenService.updateDetalle(det);
+        return ResponseEntity.ok("DetalleOrdenEntity updated successfully");
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<String> deleteDetalle(@RequestBody DetalleOrdenEntity detalle) {
+        detalleOrdenService.deleteDetalle(detalle);
+
+        DetalleOrdenEntity det = detalleOrdenService.getDetalleById(detalle.getIdDetalle());
+        if (det == null) {
+            return ResponseEntity.ok("DetalleOrdenEntity deleted successfully");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteDetalleById(@PathVariable Long id) {
+        detalleOrdenService.deleteDetalleById(id);
+
+        DetalleOrdenEntity det = detalleOrdenService.getDetalleById(id);
+        if (det == null) {
+            return ResponseEntity.ok("DetalleOrdenEntity deleted successfully");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
--- src/main/java/com/example/ecommerce/controllers/ProductoController.java
+++ src/main/java/com/example/ecommerce/controllers/ProductoController.java
@@ -1,9 +1,73 @@
 package com.example.ecommerce.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.ecommerce.entities.ProductoEntity;
+import com.example.ecommerce.services.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/productos")
 public class ProductoController {
+    @Autowired
+    ProductoService productoService;
+
+    @GetMapping("/byId/{id}")
+    public ResponseEntity<ProductoEntity> getProductoById(@PathVariable Long id) {
+        ProductoEntity prod = productoService.getProductoById(id);
+        if (prod != null) {
+            return ResponseEntity.ok(prod);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<ProductoEntity>> getAllProductos() {
+        List<ProductoEntity> prod = productoService.getAllProductos();
+        if (prod != null) {
+            return ResponseEntity.ok(prod);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<String> saveProducto(@RequestBody ProductoEntity prod) {
+        productoService.saveProducto(prod);
+        return ResponseEntity.status(HttpStatus.CREATED).body("ProductoEntity created successfully");
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<String> updateProducto(@RequestBody ProductoEntity prod) {
+        productoService.updateProducto(prod);
+        return ResponseEntity.ok("ProductoEntity updated successfully");
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteCategoriaById(@PathVariable Long id) {
+        productoService.deleteProductoById(id);
+
+        ProductoEntity prod = productoService.getProductoById(id);
+        if (prod == null) {
+            return ResponseEntity.ok("ProductoEntity deleted successfully");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<String> deleteProducto(@RequestBody ProductoEntity producto) {
+        productoService.deleteProducto(producto);
+
+        ProductoEntity prod = productoService.getProductoById(producto.getIdProducto());
+        if (prod == null) {
+            return ResponseEntity.ok("ProductoEntity deleted successfully");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
