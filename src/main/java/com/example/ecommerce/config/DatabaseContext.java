afca2a9a092af3a990f42660f3941cae2bc386c6
MatiasMontNec <matias.montano@usach.cl>
2024-11-24 17:19:18 -0300

    ID en Logs listo

--- src/main/java/com/example/ecommerce/config/DatabaseContext.java
+++ src/main/java/com/example/ecommerce/config/DatabaseContext.java
@@ -2,13 +2,21 @@ package com.example.ecommerce.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.sql2o.Sql2o;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DatabaseContext {
     @Bean
     public Sql2o sql2o() {
-        return new Sql2o("jdbc:postgresql://localhost:5432/ecommerce", "postgres", "admin");
+        return new Sql2o("jdbc:postgresql://localhost:5432/ecommerce", "postgres", "B7a742114");
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
     }
 }
 //B7a742114
