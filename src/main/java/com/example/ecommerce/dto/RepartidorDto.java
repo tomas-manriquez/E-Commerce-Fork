f5f5b4d023a4ab84fe438d48fd8e7f6cbcda7f8a
Nicolas Sepulveda <nicolas.sepulveda.b@usach.cl>
2024-12-15 21:54:43 -0300

    Fix on Repartidores by Zona List

--- src/main/java/com/example/ecommerce/dto/RepartidorDto.java
+++ src/main/java/com/example/ecommerce/dto/RepartidorDto.java
@@ -1,10 +1,10 @@
 package com.example.ecommerce.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
@@ -14,5 +14,6 @@ public class RepartidorDto {
     private Long idRepartidor;
     private String nombre;
     private String apellido;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime fechaEntrega;
 }
