1558b4870776625f9dd1a1abf14482e80e8b3345
Nicolas Sepulveda <nicolas.sepulveda.b@usach.cl>
2024-12-27 17:28:37 -0300

    Fix on create/update opinion on validate references

--- src/main/java/com/example/ecommerce/services/OpinionService.java
+++ src/main/java/com/example/ecommerce/services/OpinionService.java
@@ -1,10 +1,12 @@
 package com.example.ecommerce.services;
 
+import com.example.ecommerce.dto.OpinionDTO;
 import com.example.ecommerce.entities.OpinionEntity;
 import com.example.ecommerce.repositories.OpinionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -31,14 +33,30 @@ public class OpinionService {
         return opinionRepository.findById(id);
     }
 
-    public OpinionEntity createOpinion(OpinionEntity opinion) {
-        validateReferences(opinion);
+    public OpinionEntity createOpinion(OpinionDTO op) {
+        validateReferences(op);
+        OpinionEntity opinion = new OpinionEntity();
+        opinion.setComentario(op.getComentario());
+        opinion.setFecha(LocalDateTime.now());
+        opinion.setPuntuacion(op.getPuntuacion());
+        opinion.setIdCliente(op.getIdCliente());
+        opinion.setIdProducto(op.getIdProducto());
+        opinion.setIdCategoria(op.getIdCategoria());
         return opinionRepository.save(opinion);
     }
 
-    public OpinionEntity updateOpinion(String id, OpinionEntity opinion) {
-        opinion.setIdOpinion(id);
-        validateReferences(opinion);
+    public OpinionEntity updateOpinion(String id, OpinionDTO op) {
+        OpinionEntity opinion = opinionRepository.findById(id).get();
+        if (opinion == null) {
+            throw new RuntimeException("Opinion no encontrads");
+        }
+        validateReferences(op);
+        opinion.setComentario(op.getComentario());
+        opinion.setFecha(LocalDateTime.now());
+        opinion.setPuntuacion(op.getPuntuacion());
+        opinion.setIdCliente(op.getIdCliente());
+        opinion.setIdProducto(op.getIdProducto());
+        opinion.setIdCategoria(op.getIdCategoria());
         return opinionRepository.save(opinion);
     }
 
@@ -46,7 +64,7 @@ public class OpinionService {
         opinionRepository.deleteById(id);
     }
 
-    private void validateReferences(OpinionEntity opinion) {
+    private void validateReferences(OpinionDTO opinion) {
         if (!productoService.existsById(opinion.getIdProducto())) {
             throw new IllegalArgumentException("idProducto inválido");
         }
