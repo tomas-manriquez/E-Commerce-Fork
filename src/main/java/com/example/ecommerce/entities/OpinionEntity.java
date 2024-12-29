849793bba76ba4232b70924278c116edd37ea54d
Josue Quileñan <josue.quilenan@usach.cl>
2024-12-29 18:29:39 -0300

    Me había pitiado la reseña xd, perdón

--- src/main/java/com/example/ecommerce/entities/OpinionEntity.java
+++ src/main/java/com/example/ecommerce/entities/OpinionEntity.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @Document(collection = "opiniones")
 public class OpinionEntity {
     @Id
-    private Long idOpinion;
+    private String idOpinion;
 
     private Long idCliente;
     private Long idProducto;
